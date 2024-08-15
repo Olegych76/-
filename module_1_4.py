199928f0942376e54f3c60768b1e034d64337597
Olegych76 <f-o-r-j-o-b@yandex.ru>
2024-08-15 14:03:35 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,10 @@
+my_string = input("Введите произвольную строку: ")
+print("Длина строки:", len(my_string))
+
+print("Строка в нижнем регистре:", my_string.lower())
+print("Строка в верхнем регистре:", my_string.upper())
+
+print("Удалим пробелы из строки:", my_string.replace(' ', ''))
+
+print("Первый символ строки:", my_string[0])
+print("Последний символ строки:", my_string[-1])
